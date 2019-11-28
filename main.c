81aa55d29d92a47ded441fc2c8270ebdeb2f6cf1
yogev100 <yogev2468@gmail.com>
2019-11-28 10:51:54 +0200

    update ex2-without buffer

--- main.c
+++ main.c
@@ -12,19 +12,12 @@ int main(){
         int accountNum;//input of account number
         double amount;//input of amount
         double rate;//input of rate
-        printf("\nTransaction type?:");
-        scanf(" %c",&transType);
-        if(getchar()!='\n'){//if the input is clean
+        printf("\nTransaction type?:\n");
+        if(scanf(" %c",&transType)!=1){
             printf("You have inserted a wrong character, please try again!\n");
-            while(getchar()!='\n');//(clears the input buffer) 
             continue;
         }
-        //if the input is valid - its will pass the 'if case' and arrive to the switch cases.
-        if(transType!='O' &&transType!='B' &&transType!='D' &&transType!='W' &&transType!='C' &&transType!='P' &&transType!='I' &&transType!='E')
-        {
-            printf("You have inserted a wrong character, please try again!\n");//invalid input
-            continue;
-        }
+        else {
         
         switch(transType)//the standard cases, each case check if the input is valid , and if not - returns to the next iteration.
         {
@@ -32,10 +25,8 @@ int main(){
             printf("Initial deposit?:\n");
             if(scanf("%lf",&amount)==1){
                 newAccount(amount);
-                while(getchar()!='\n');
             }
             else{
-                while(getchar()!='\n');               
                 printf("You have inserted a wrong amount, please try again!\n");
             }
             break;
@@ -43,10 +34,8 @@ int main(){
             printf("Account number?:\n");
             if(scanf("%d",&accountNum)==1){
                 balance(accountNum);
-                while(getchar()!='\n');
             }
             else {
-                while(getchar()!='\n');
                 printf("You have inserted a wrong account number, please try again!\n");
             }
 
@@ -54,38 +43,30 @@ int main(){
         case 'D':
             printf("‫‪Account_number‬‬?:\n");
             if(scanf("%d",&accountNum)==1){
-                while(getchar()!='\n');
                 printf("‫‪Amount?:\n");
                 if(scanf("%lf",&amount)==1){
                     deposit(accountNum,amount);
-                    while(getchar()!='\n');
                 }
                 else {
-                    while(getchar()!='\n');
                     printf("You have inserted a wrong amount, please try again!\n");                    
                 }
             }
             else {
-                while(getchar()!='\n');
                 printf("You have inserted a wrong account number, please try again!\n");
             }
             break;
         case 'W':
             printf("‫‪Account_number‬‬?:\n");
             if(scanf("%d",&accountNum)==1){
-                while(getchar()!='\n');
                 printf("‫‪Amount?:\n");
                 if(scanf("%lf",&amount)==1){
                     cashWithdraw(accountNum,amount);
-                    while(getchar()!='\n');
                 }
                 else {
-                    while(getchar()!='\n');
                     printf("You have inserted a wrong amount, please try again!\n");
                 }
             }
             else {
-                while(getchar()!='\n');
                 printf("You have inserted a wrong account number, please try again!\n");
             }
             break;
@@ -93,10 +74,8 @@ int main(){
             printf("Account number?:\n");
             if(scanf("%d",&accountNum)==1){
                 closeAccount(accountNum);
-                while(getchar()!='\n');
             }
             else {
-                while(getchar()!='\n');
                 printf("You have inserted a wrong account number, please try again!\n");
             }       
 
@@ -105,10 +84,8 @@ int main(){
             printf("‫‪‬‬ ‫‪Interest rate?:\n");
             if(scanf("%lf",&rate)==1){
                 interestRate(rate);
-                while(getchar()!='\n');
             }
             else{
-                while(getchar()!='\n');
                 printf("You have inserted a wrong rate, please try again!\n");
             }
             break;
@@ -124,9 +101,9 @@ int main(){
         default:
             printf("Invalid transaction\n");
             break;
+            }
         }
     }
 
     return 0;
 }
-
--- test.c
+++ test.c
@@ -1,74 +1,95 @@
 #include <stdio.h>
 #include "myBank.h"
 
-enum bool{no,yes};
 
+//////////////////////without buffer/////
+
+enum bool{no,yes};//a new type variable for knowing when to close all the accounts.
 
 int main(){
     enum bool exit=no;
 
-    // do{
-    //      char transType;
-    //      printf("\nTransaction type?:");
-    //      int input=scanf(" %c",&transType);
-    //      printf("\n"); 
-    while()
     while(exit==no){
-        char transType;
-        int accountNum;
-        double amount;
-        double rate;
+        char transType;//input of transaction
+        int accountNum;//input of account number
+        double amount;//input of amount
+        double rate;//input of rate
         printf("\nTransaction type?:");
-        while(scanf(" %c",&transType)!=1){
-        // while(transType!='O' &&transType!='B' &&transType!='D' &&transType!='W' &&transType!='C' &&transType!='P' &&transType!='I' &&transType!='E')
-        // {
+        if(scanf(" %c",&transType)!=1){
             printf("You have inserted a wrong character, please try again!\n");
-            printf("\nTransaction type?");
-            scanf(" %c",&transType); 
-
-         }
-        printf("\n");
-        switch(transType)
+            continue;
+        }
+        else {
+        
+        switch(transType)//the standard cases, each case check if the input is valid , and if not - returns to the next iteration.
         {
         case 'O':
-            printf("Initial deposit?:");
-            scanf("%lf",&amount);
-            printf("\n");
-            newAccount(amount);
+            printf("Initial deposit?:\n");
+            if(scanf("%lf",&amount)==1){
+                newAccount(amount);
+            }
+            else{
+                printf("You have inserted a wrong amount, please try again!\n");
+            }
             break;
         case 'B':
-            printf("Account number?:");
-            scanf("%d",&accountNum);
-            printf("\n");
-            balance(accountNum);
+            printf("Account number?:\n");
+            if(scanf("%d",&accountNum)==1){
+                balance(accountNum);
+            }
+            else {
+                printf("You have inserted a wrong account number, please try again!\n");
+            }
+
             break;
         case 'D':
-            printf("‫‪Account_number‬‬?:");
-            scanf("%d",&accountNum);
-            printf("\n");
-            printf("‫‪Amount?:");
-            scanf("%lf",&amount);
-            printf("\n");
-            deposit(accountNum,amount);
+            printf("‫‪Account_number‬‬?:\n");
+            if(scanf("%d",&accountNum)==1){
+                printf("‫‪Amount?:\n");
+                if(scanf("%lf",&amount)==1){
+                    deposit(accountNum,amount);
+                }
+                else {
+                    printf("You have inserted a wrong amount, please try again!\n");                    
+                }
+            }
+            else {
+                printf("You have inserted a wrong account number, please try again!\n");
+            }
             break;
         case 'W':
-            printf("‫‪Account_number‬‬?:");
-            scanf("%d",&accountNum);
-            printf("\n");
-            printf("‫‪Amount?:");
-            scanf("%lf",&amount);
-            printf("\n");
-            cashWithdraw(accountNum,amount);           
+            printf("‫‪Account_number‬‬?:\n");
+            if(scanf("%d",&accountNum)==1){
+                printf("‫‪Amount?:\n");
+                if(scanf("%lf",&amount)==1){
+                    cashWithdraw(accountNum,amount);
+                }
+                else {
+                    printf("You have inserted a wrong amount, please try again!\n");
+                }
+            }
+            else {
+                printf("You have inserted a wrong account number, please try again!\n");
+            }
             break;
         case 'C':
-            printf("Account number?:");
-            scanf("%d",&accountNum);
-            closeAccount(accountNum);
+            printf("Account number?:\n");
+            if(scanf("%d",&accountNum)==1){
+                closeAccount(accountNum);
+            }
+            else {
+                printf("You have inserted a wrong account number, please try again!\n");
+            }       
+
             break;
         case 'I':
-            printf("‫‪‬‬ ‫‪Interest rate?:");
-            scanf("%lf",&rate);
-            interestRate(rate);
+            printf("‫‪‬‬ ‫‪Interest rate?:\n");
+            if(scanf("%lf",&rate)==1){
+                interestRate(rate);
+            }
+            else{
+                printf("You have inserted a wrong rate, please try again!\n");
+            }
             break;
         case 'P':
             accountsDetails();
@@ -80,7 +101,141 @@ int main(){
             break;
             
         default:
-            printf("Invalid transaction");
+            printf("Invalid transaction\n");
+            break;
+            }
+        }
+    }
+
+    return 0;
+}
+
+//////////////////////////////////////////with buffer////////////////////////////////
+#include <stdio.h>
+#include "myBank.h"
+
+
+enum bool{no,yes};//a new type variable for knowing when to close all the accounts.
+
+int main(){
+    enum bool exit=no;
+
+    while(exit==no){
+        char transType;//input of transaction
+        int accountNum;//input of account number
+        double amount;//input of amount
+        double rate;//input of rate
+        printf("\nTransaction type?:");
+        scanf(" %c",&transType);
+        if(getchar()!='\n'){//if the input is clean
+            printf("You have inserted a wrong character, please try again!\n");
+            while(getchar()!='\n');//(clears the input buffer) 
+            continue;
+        }
+        //if the input is valid - its will pass the 'if case' and arrive to the switch cases.
+        if(transType!='O' &&transType!='B' &&transType!='D' &&transType!='W' &&transType!='C' &&transType!='P' &&transType!='I' &&transType!='E')
+        {
+            printf("You have inserted a wrong character, please try again!\n");//invalid input
+            continue;
+        }
+        
+        switch(transType)//the standard cases, each case check if the input is valid , and if not - returns to the next iteration.
+        {
+        case 'O':
+            printf("Initial deposit?:\n");
+            if(scanf("%lf",&amount)==1){
+                newAccount(amount);
+                while(getchar()!='\n');
+            }
+            else{
+                while(getchar()!='\n');               
+                printf("You have inserted a wrong amount, please try again!\n");
+            }
+            break;
+        case 'B':
+            printf("Account number?:\n");
+            if(scanf("%d",&accountNum)==1){
+                balance(accountNum);
+                while(getchar()!='\n');
+            }
+            else {
+                while(getchar()!='\n');
+                printf("You have inserted a wrong account number, please try again!\n");
+            }
+
+            break;
+        case 'D':
+            printf("‫‪Account_number‬‬?:\n");
+            if(scanf("%d",&accountNum)==1){
+                while(getchar()!='\n');
+                printf("‫‪Amount?:\n");
+                if(scanf("%lf",&amount)==1){
+                    deposit(accountNum,amount);
+                    while(getchar()!='\n');
+                }
+                else {
+                    while(getchar()!='\n');
+                    printf("You have inserted a wrong amount, please try again!\n");                    
+                }
+            }
+            else {
+                while(getchar()!='\n');
+                printf("You have inserted a wrong account number, please try again!\n");
+            }
+            break;
+        case 'W':
+            printf("‫‪Account_number‬‬?:\n");
+            if(scanf("%d",&accountNum)==1){
+                while(getchar()!='\n');
+                printf("‫‪Amount?:\n");
+                if(scanf("%lf",&amount)==1){
+                    cashWithdraw(accountNum,amount);
+                    while(getchar()!='\n');
+                }
+                else {
+                    while(getchar()!='\n');
+                    printf("You have inserted a wrong amount, please try again!\n");
+                }
+            }
+            else {
+                while(getchar()!='\n');
+                printf("You have inserted a wrong account number, please try again!\n");
+            }
+            break;
+        case 'C':
+            printf("Account number?:\n");
+            if(scanf("%d",&accountNum)==1){
+                closeAccount(accountNum);
+                while(getchar()!='\n');
+            }
+            else {
+                while(getchar()!='\n');
+                printf("You have inserted a wrong account number, please try again!\n");
+            }       
+
+            break;
+        case 'I':
+            printf("‫‪‬‬ ‫‪Interest rate?:\n");
+            if(scanf("%lf",&rate)==1){
+                interestRate(rate);
+                while(getchar()!='\n');
+            }
+            else{
+                while(getchar()!='\n');
+                printf("You have inserted a wrong rate, please try again!\n");
+            }
+            break;
+        case 'P':
+            accountsDetails();
+            break;
+        case 'E':
+            closeAll();
+            printf("All the accounts are closed\n");
+            exit=yes;
+            break;
+            
+        default:
+            printf("Invalid transaction\n");
             break;
         }
     }
